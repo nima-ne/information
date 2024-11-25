fb8b92ab4c1d6cf851acef096dcd2eea61eafb42
unknown <nimaneshat44@gmail.com>
2024-11-25 18:58:57 +0330

    first commit

---
+++ js/info3.js
@@ -0,0 +1,9 @@
+let firstname = "amirhossein"
+let lastname = "rashidi"
+let age = 20
+let current_persian_date = 1403
+let current_miladi_date = 2024
+
+document.write(`<h1> youre name is ${firstname} ${lastname} and you are ${age} years old</h1>`)
+document.write(`<h1>youre birth year is ${current_persian_date - age} (shimsi calendar)</h1>`)
+document.write(`<h1>youre birth year is ${current_miladi_date - age} (miladi calendar)</h1>`)
---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstname = "nima"
+let lastname = "neshat"
+let age = 22
+let current_persian_date = 1403
+let current_miladi_date = 2024
+
+document.write(`<h1> youre name is ${firstname} ${lastname} and you are ${age} years old</h1>`)
+document.write(`<h1>youre birth year is ${current_persian_date - age} (shimsi calendar)</h1>`)
+document.write(`<h1>youre birth year is ${current_miladi_date - age} (miladi calendar)</h1>`)
